b7f0f6e2df01d88f63af644590ec1300c5318352
Solomon <solobarine@gmail.com>
2023-05-05 18:52:10 +0100

    Fix Linter errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    it 'Updates Likes Counter in Post' do
-        author = User.create(
-            name: 'Bobby',
-            photo: 'http://photo.com',
-            bio: 'A proud man',
-            posts_counter: 6
-        )
+  it 'Updates Likes Counter in Post' do
+    author = User.create(
+      name: 'Bobby',
+      photo: 'http://photo.com',
+      bio: 'A proud man',
+      posts_counter: 6
+    )
 
-        post = Post.create(
-            title: 'Fake Post',
-            text: 'Nonsense',
-            comments_counter: 2,
-            likes_counter: 0,
-            author: author
-        )
+    post = Post.create(
+      title: 'Fake Post',
+      text: 'Nonsense',
+      comments_counter: 2,
+      likes_counter: 0,
+      author:
+    )
 
-        Like.create!(
-            author: author,
-            post: post
-        )
-        expect(post.likes_counter).to eq(1)
-    end
+    Like.create!(
+      author:,
+      post:
+    )
+    expect(post.likes_counter).to eq(1)
+  end
 end
